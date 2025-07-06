81747444f82bc6b0df00989683b5848a3b279306
adityak814 <aditya.kumaraug814@gmail.com>
2025-07-07 02:03:05 +0530

    mongodb and clerk setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+
+//Middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks);
+
+app.get("/", (req, res) => {
+  res.send("API is working fine");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT: ${PORT}`);
+});
