08d4c4032b5f8f0ff6e84109953ce0bea4da3c03
adityak814 <aditya.kumaraug814@gmail.com>
2025-07-11 01:49:39 +0530

    setup hotel registration feature

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import { useState } from "react";
 import { assets, cities } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const HotelReg = () => {
+  const { setShowHotelReg, axios, setIsOwner, getToken } = useAppContext();
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [city, setCity] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const { data } = await axios.post(
+        `/api/hotels`,
+        { name, contact, address, city },
+        { headers: { Authorization: `Bearer ${await getToken()}` } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setIsOwner(true);
+        setShowHotelReg(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70">
-      <form className="flex bg-white rounded-xl max-w-4xl max-md:mx-2">
+    <div
+      onClick={() => setShowHotelReg(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70"
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex bg-white rounded-xl max-w-4xl max-md:mx-2"
+      >
         <img
           src={assets.regImage}
           alt="reg-image"
@@ -16,6 +53,7 @@ const HotelReg = () => {
             src={assets.closeIcon}
             alt="close-icon"
             className="absolute top-4 right-4 h-4 w-4 cursor-pointer"
+            onClick={() => setShowHotelReg(false)}
           />
           <p className="text-2xl font-semibold mt-6">Register Your Hotel</p>
           {/* Hotel Name */}
@@ -29,6 +67,8 @@ const HotelReg = () => {
               placeholder="Type here"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
               required
+              onChange={(e) => setName(e.target.value)}
+              value={name}
             />
           </div>
           {/* Phone */}
@@ -42,6 +82,8 @@ const HotelReg = () => {
               placeholder="Type here"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
               required
+              onChange={(e) => setContact(e.target.value)}
+              value={contact}
             />
           </div>
           {/* Address */}
@@ -55,6 +97,8 @@ const HotelReg = () => {
               placeholder="Type here"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
               required
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
             />
           </div>
           {/* Select City Drop Down */}
@@ -66,6 +110,8 @@ const HotelReg = () => {
               id="city"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
               required
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
             >
               <option value="">Select City</option>
               {cities.map((city) => (
