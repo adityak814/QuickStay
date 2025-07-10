9f761371c0f92a1a219adcfed2ad4f11b70c4e05
adityak814 <aditya.kumaraug814@gmail.com>
2025-07-11 03:01:51 +0530

    setup owner dashboard page

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 
 const ListRoom = () => {
   const [rooms, setRooms] = useState([]);
-  const { axios, getToken, user } = useAppContext();
+  const { axios, getToken, user, currency } = useAppContext();
 
   // Fetch Rooms of the Hotel Owner
   const fetchRooms = async () => {
@@ -92,7 +92,7 @@ const ListRoom = () => {
                   {item.amenities.join(", ")}
                 </td>
                 <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-left">
-                  {item.pricePerNight}
+                  {currency} {item.pricePerNight}
                 </td>
                 <td className="py-3 px-4 text-red-500 border-t border-gray-300 text-center text-sm">
                   <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
